ca0e813ec3dbb5f9ec2e82ffdb84b61de37a5f64
Daniel Tello <38147818+danieltello3@users.noreply.github.com>
2023-11-28 15:05:48 -0500

    cambios en ruta de orden

--- src/routes/orden.routes.js
+++ src/routes/orden.routes.js
@@ -5,4 +5,4 @@ export const ordenRouter = Router();
 
 ordenRouter.route("/orden").post(getUsuarioAutenticado, crearOrden)
 ordenRouter.route("/orden/:orden_id").get()
-ordenRouter.route("/orden/:comprador_id").get(ordenUsuario)
+ordenRouter.route("/orden/usuario/:comprador_id").get(ordenUsuario)
